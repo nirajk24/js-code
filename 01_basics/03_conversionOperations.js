e54c889acb44e173abe57b0e6d14c965c679bb1e
Niraj <101890608+nirajk24@users.noreply.github.com>
2024-06-16 16:24:03 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+// string will give NaN
+// null will give 0
+// undefined will give NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 40
+let someString = String(someNumber)
+
+console.log(someString)
+console.log(typeof(someString))
